6350d9ef28d679853055b73ddf63c2ca7080babf
Jose Alonso Añonuevo <jalonso@tcpsi.es>
2021-07-31 16:32:43 +0200

    Subida inicial

---
+++ main.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_providers {
+    spotify = {
+      version = "~> 0.1.5"
+      source  = "conradludgate/spotify"
+    }
+  }
+}
+
+provider "spotify" {
+  api_key = var.spotify_api_key
+}
+
+resource "spotify_playlist" "playlist" {
+  name        = "Test"
+  description = "This playlist was created by Terraform"
+  public      = false
+
+  tracks = [
+    data.spotify_search_track.by_artist.tracks[0].id,
+    data.spotify_search_track.by_artist.tracks[1].id,
+    data.spotify_search_track.by_artist.tracks[2].id,
+  ]
+}
+
+data "spotify_search_track" "by_artist" {
+  artists = ["Metallica"]
+  album = "Master of puppets"
+  #name  = "Battery"
+}
+
